bc68a7e710167552cb41c5239343b60e887b443f
Shakib448 <shakiba448@gmail.com>
2020-08-27 03:41:53 +0600

    React Online Course App

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -4,6 +4,11 @@ import FakeData from "../FakeData/FakeData";
 import { useState } from "react";
 import EnrollCourse from "../EnrollCourse/EnrollCourse";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+const icon = <FontAwesomeIcon icon={faArrowDown} />
+
 const Course = () => {
 
     const [BookCourse, setBookCourse] = useState(FakeData);
@@ -15,6 +20,8 @@ const Course = () => {
         setEnrollCourse(newEnroll);
     }
 
+
+
     
     const total = enrollCourse.reduce((total, add)=> total + parseInt(add.price.replace(/,/g, '')) , 0)
 
@@ -23,7 +30,8 @@ const Course = () => {
             <div className="my-5 text-center">
                 <h1 className="btn btn-outline-primary mr-2">Total Courses {BookCourse.length} </h1>
                 <h1 className="btn btn-outline-primary">Total Enrolled {enrollCourse.length} </h1> <br/>
-                <h1 className="btn btn-outline-primary">Total Enrolled Cost {total} </h1>
+                <h1 className="btn btn-outline-primary">Total Enrolled Cost {total} </h1> <br/>
+                <h1 className= "upDown"> {icon} </h1> 
             </div>
             <div className="container-fluid mb-5">
                 <div className="row">
@@ -38,19 +46,24 @@ const Course = () => {
                                     />
                                 })
                             }
+                            <div className="col-md-12 text-center">
+                                <h1>Enrolled Details</h1>
+                                <hr/>
+                            </div>
                             {/* For Buy Course  */}
-                            <div>
+
                             {
                                 enrollCourse.map((buyCourse, index) =>{
                                 return <EnrollCourse
                                 img={buyCourse.imgsrc} 
                                 title={buyCourse.title}
                                 buyCount={enrollCourse.length}
+                                price={buyCourse.price}
                                 key={index}/>
                                 })
 
                             }
-                            </div>
+
 
                         </div>
                     </div>
--- src/Components/EnrollCourse/EnrollCourse.js
+++ src/Components/EnrollCourse/EnrollCourse.js
@@ -6,13 +6,12 @@ const EnrollCourse = (props) => {
     return (
         <>
             <div className="col-md-4 col-10">
-                <h1></h1>
                 <div className="card">
                     <img  src={props.img} className="card-img-top" alt="{props.imgsrc}"/>
                     <div className="card-body">
                         <h5 className="card-title font-weight-bold">{props.title}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content </p>
-                        <h1>Total buy Course </h1>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="btn-outline-primary"> {props.price} </p>
                     </div>
                 </div>
 
